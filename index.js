0077f5c9098daea9db364b55d3a07d9a0c8f9ffa
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-31 20:32:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,240 @@
+
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+const port = process.env.PORT || 5000
+const cors = require('cors')
+app.use(cors({
+   origin: [
+     'http://localhost:5173'
+   ],
+   credentials: true
+}))
+app.use(express.json())
+app.use(cookieParser())
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f8w8siu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  
+  
+
+  const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  };
+
+  async function run() {
+    try { 
+    const userCollection = client.db('accountDB').collection('accounts')
+    const mobileCollection = client.db('accountDB').collection('mobile')
+
+    const verifyToken = async(req,res,next)=>{
+      if(!req.headers.authorization){
+           return res.status(401).send({messsage:'forbidden access'})
+      }
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+          if(err){
+             return res.status(401).send({message:'forbidden access'})
+          }
+          req.decoded = decoded
+          next()
+      })
+        //  const token = req.cookies?.token 
+        // //  console.log('value of verify token',token);
+        //  if(!token){
+        //    return res.status(401).send({message: 'not autorized'})
+        //  }
+        //  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        //   if(err){
+        //     console.log(err);
+        //     return res.status(401).send({message: 'unauthorized'})
+        //   }
+        //   req.user = decoded
+        //   next()
+        //  })
+       
+         
+    }
+  
+
+    app.post('/jwt',async(req,res)=>{
+       const user = req.body 
+       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET ,{expiresIn: '1000h'})
+       res
+       .cookie('token',token,cookieOptions)
+       .send({token}) 
+      //  .send({success:true})
+    })
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+    app.post('/logout',async(req,res)=>{
+        const user = req.body;
+        res.clearCookie('token',{...cookieOptions, maxAge:0}).send({success:true})
+
+    })
+
+    app.get('/users',verifyToken,async(req,res)=>{
+      const result = await userCollection.find().toArray()
+      res.send(result)
+  })
+
+  app.get('/users/admin/:email', verifyToken, async (req, res) => {
+    const email = req.params.email;
+    if (email !== req.decoded.email) {
+      return res.status(403).send({ message: 'forbidden access' })
+    }
+
+    const query = { email: email };
+    const user = await userCollection.findOne(query);
+    let admin = false;
+    if (user) {
+      admin = user?.role === 'admin';
+    }
+    res.send({ admin });
+  })
+   
+    app.get('/users',verifyToken,verifyAdmin,async(req,res)=>{
+        const cursor = userCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.post('/users',async(req,res)=>{
+      const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query)
+      if(existingUser){
+        return res.send({message:'user already exists'})
+      }
+      const cartItem = req.body; 
+      const result = await userCollection.insertOne(cartItem)
+      res.send(result)
+ })
+
+ app.delete('/users/:id',async(req,res)=>{
+  const id = req.params.id; 
+  const query = {
+    _id: new ObjectId(id) 
+  }
+  const result = await userCollection.deleteOne(query)
+  res.send(result)
+})
+
+app.patch('/users/admin/:id',verifyToken,verifyAdmin, async(req,res)=>{
+ const id = req.params.id; 
+ const filter = {_id : new ObjectId(id)}
+ const updateDoc = {
+    $set:{
+       role: 'admin'
+    }
+ }
+ const result = await userCollection.updateOne(filter,updateDoc)
+ res.send(result)
+
+})
+  
+   
+    
+    app.get('/productsCount',async(req,res)=>{
+        
+      const count = await CoffeeCollection.estimatedDocumentCount();
+      res.send({count});
+ })
+
+ app.get('/products',async(req,res)=>{
+      
+  const page = parseInt(req.query.page)
+  const size = parseInt(req.query.size)
+
+
+   const cursor = CoffeeCollection.find()
+   const result = await cursor.skip(page * size).limit(size).toArray()
+   res.send(result)
+})
+
+// paymnet 
+
+app.post('/create-payment-intent',async(req,res)=>{
+  const {price} = req.body 
+
+  const amount = parseInt(price * 100)
+  console.log(amount,'inside the amount')  
+  const paymentIntent = await stripe.paymentIntents.create({
+      amount:amount, 
+      currency:'usd',
+      payment_method_types:[
+         'card',
+      ],  
+   })
+
+   res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+  
+
+});
+
+
+app.get('/mobile',async(req,res)=>{
+  const cursor = mobileCollection.find()
+  const result = await cursor.toArray()
+  res.send(result)
+})
+
+
+
+
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      
+    }
+  }
+  run().catch(console.dir);
+  
+  
+  
+  
+  
+  
+  app.get('/', (req, res) => {
+      res.send('Hello World! it s me how are you i am localhost')
+    })
+  
+  
+  
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
+  
+    
